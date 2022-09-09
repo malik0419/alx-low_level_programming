b64254ee40c32b2ae4f405fbc15e742013c58c72
malik <maliksunday592@gmail.com>
2022-09-08 19:14:20 -0700

    Last last commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %ld byte(s)\n", sizeof(char));
-	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of float: %ld byte(s)\n", sizeof(float));
+	printf("Size of char: %ld byte\n", sizeof(char));
+	printf("Size of an int: %ld bytes\n", sizeof(int));
+	printf("Size of a long int: %ld bytes\n", sizeof(long int));
+	printf("Size of long long int: %ld bytes\n", sizeof(long long int));
+	printf("Size of float: %ld bytes\n", sizeof(float));
 	return (0);
 }
