17a5e5482d92dea76fa9fd83c3456bfb35e7730d
malik <maliksunday592@gmail.com>
2022-09-11 07:03:18 -0700

    .

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,13 @@ int main(void)
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-		putchar(i);
-	for (i = 'A'; i <= 'Z'; i++)
-		putchar(i);
+		if ((i == 'e') || (i == 'q'))
+		{
+		}
+		else
+		{
+			putchar(i);
+		}
 	putchar('\n');
-
 	return (0);
 }
