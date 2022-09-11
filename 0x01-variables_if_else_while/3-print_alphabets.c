d9d04f4f883ca4d18b5d2b5353804264ef220457
malik <maliksunday592@gmail.com>
2022-09-11 07:00:01 -0700

    .

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) success;
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if ((i == 'e') || (i == 'q'))
+		{
+		}
+		else
+		{
+			putchar(i);
+		}
+	putchar('\n');
+	return (0);
+}
