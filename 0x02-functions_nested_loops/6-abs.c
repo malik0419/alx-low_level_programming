26d245a92539530962c9f290abb5d01bf6bb00c0
malik <maliksunday592@gmail.com>
2022-09-16 11:31:01 -0700

    corrections

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * _abs - function that computes the absolute
+ *	value of integer
+ *
+ * @n: takes integer type input for function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
